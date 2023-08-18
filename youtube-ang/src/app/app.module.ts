2de7508fbb4210c4fbbe3555ae084c3216a5f155
kaustubhdeokar <kaustubhd9@gmail.com>
2023-08-19 01:03:19 +0530

    completed todo app

--- youtube-ang/src/app/app.module.ts
+++ youtube-ang/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { XyzComponent } from './xyz/xyz.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    XyzComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
